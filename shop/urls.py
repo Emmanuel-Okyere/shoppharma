b7a278095f258011eef72c6d487077e116ae2316
emmanuel-gyateng <57065066+Emmanuel-Okyere@users.noreply.github.com>
2022-06-19 13:58:18 +0000

    Upgraded the payment html

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,7 @@ from shop import views
 app_name = "shop"
 urlpatterns = [
     path("shop/", views.product_list, name = "shop"),
+    path("shop/notfount", views.page_not_found, name = "404"),
     path("shop/<slug:category_slug>/", views.product_list, name = "product_list_by_category"),
     path("<int:id>/<slug:slug>/detail/", views.product_detail, name = "product_detail"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import redirect, render,get_object_or_404
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger,InvalidPage
+from django.urls import reverse
 from .models import Product, Category
 from cart.forms import CartAddProductForm
+
 # Create your views here.
 
 def product_list(request, category_slug=None):
@@ -8,11 +11,21 @@ def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    paginator = Paginator(products, 9)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts= paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
     return render(request, "shop/index.html", {"category": category,
-                                                      "categories": categories, "products": products})
+                                                      "categories": categories,
+                                                      "products": products,
+                                                      "post":posts,"page":page})
 
 
 def product_detail(request, id, slug):
@@ -22,3 +35,8 @@ def product_detail(request, id, slug):
     cart_product_form = CartAddProductForm()
     return render(request, "shop/details.html", {"product": product,"categories":categories,
     "cart_product_form":cart_product_form})
+
+
+def page_not_found(request):
+    """When payment goes through"""
+    return render(request, "shop/404.html")
