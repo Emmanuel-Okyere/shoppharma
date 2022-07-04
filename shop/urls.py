6016e79dfb71ab3f7cb799230ca3978546cd7232
emmanuel-gyateng <57065066+Emmanuel-Okyere@users.noreply.github.com>
2022-07-04 20:02:48 +0000

    Implemented the search form

--- shop/urls.py
+++ shop/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("adminshop/category", views.CategoryAddSerializer.as_view(), name = "postcategory"),
     path("shop/<slug:category_slug>/", views.product_list, name = "product_list_by_category"),
     path("<int:id>/<slug:slug>/detail/", views.product_detail, name = "product_detail"),
+    path("search/" ,views.post_search, name = "post_search")
 ]
